e673cff02cc108825bc0409ce3d1adbe4ba4b8e9
FRANCISCO NAHUEL FONSECA <francisco60036@hotmail.com>
2025-09-06 16:23:23 -0300

    Las clases Marca y Categoria cambian del modificador de acceso de internal a public

--- TPWinForm_equipo-18B/Dominio/Articulo.cs
+++ TPWinForm_equipo-18B/Dominio/Articulo.cs
@@ -11,8 +11,8 @@ namespace Dominio
     {
         public int idArticulo { get; set; }
         public string nombre { get; set; }
-        //public Marca marca { get; set; }
-        //public Categoria categoria { get; set; }
+        public Marca marca { get; set; }
+        public Categoria categoria { get; set; }
         public string descripcion { get; set; }
 
         //List < imagen>
