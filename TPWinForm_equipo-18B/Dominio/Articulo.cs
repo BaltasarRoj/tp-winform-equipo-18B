d49bddac8d4a96551179e60cdcb8013c23a9c86d
BaltasarRoj <108036956+BaltasarRoj@users.noreply.github.com>
2025-09-06 14:07:56 -0300

    primera subida..

---
+++ TPWinForm_equipo-18B/Dominio/Articulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int idArticulo { get; set; }
+        public string nombre { get; set; }
+        //public Marca marca { get; set; }
+        //public Categoria categoria { get; set; }
+        public string descripcion { get; set; }
+
+        //List < imagen>
+        private float precio;
+        public float Precio
+        {
+            get { return precio; }
+            set
+            {
+                if (value <= 0)
+                    precio = value;
+
+                else precio = -2;
+            }
+
+        }
+    }
+}
+
