d02c9b1f36a06a89850140e028ea976716ac2dfc
BaltasarRoj <108036956+BaltasarRoj@users.noreply.github.com>
2025-09-10 23:01:58 -0300

    se agregaron  Annotations para la lista de articulos

--- TPWinForm_equipo-18B/Dominio/Articulo.cs
+++ TPWinForm_equipo-18B/Dominio/Articulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,12 +10,25 @@ namespace Dominio
 {
     public class Articulo
     {
+        [DisplayName("ID")]
         public int id { get; set; }
+
+        [DisplayName("CODIGO ARTICULO")]
         public string codigoArticulo { get; set; }
+
+        [DisplayName("NOMBRE")]
         public string nombre { get; set; }
+
+        [DisplayName("MARCA")]
         public Marca Marca { get; set; }
+
+        [DisplayName("TIPO")]
         public Categoria tipo { get; set; }
+
+        [DisplayName("DESCRIPCION")]
         public string descripcion { get; set; }
+
+        [DisplayName("PRECIO")]
         public decimal precio { get; set; }
         public string UrlImagen { get; set; }
 
